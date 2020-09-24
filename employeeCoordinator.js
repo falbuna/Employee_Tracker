df3449d7c1abe2f4ad17108ee6902043e586ccf5
Francis Albuna <falbuna1@gmail.com>
2020-09-24 10:48:10 -0700

    Got the Add Department function to work and update the SQL database.

--- employeeCoordinator.js
+++ employeeCoordinator.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql');
-const initialPrompt = require("./inquirerPrompts")
+const inquirer = require('inquirer');
+const viewAllEmployees = require("./lib/allEmployees");
+const viewAllDepartments = require("./lib/allDepartments");
+const viewAllRoles = require("./lib/allRoles");
+// const addDepartment = require("./lib/addDepartment");
 
 const connectionConfig = {
     host: "localhost",
@@ -15,5 +19,65 @@ connection.connect(function(err){
     if (err) throw err;
     initialPrompt();
 });
+
+function initialPrompt(){
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all Employees",
+                "View all Departments",
+                "View all Roles",
+                "Add a Department",
+                "Add a Role",
+                "Add an Employee",
+                "Update Employee Roles",
+                "Exit"
+            ]
+        }).then(function(answer) {
+                if (answer.action === "View all Employees"){
+                    viewAllEmployees();
+                    initialPrompt();
+                }
+                if(answer.action === "View all Departments"){
+                    viewAllDepartments();
+                    initialPrompt();
+                }
+                if(answer.action === "View all Roles"){
+                    viewAllRoles();
+                    initialPrompt();
+                }
+                else if(answer.action === "Add a Department"){
+                    addDepartment();
+                }
+                }
+            )
+};
+
+function addDepartment(){
+    inquirer
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "What is the name of the department you want add?"
+            }
+        ])
+        .then(function(response){
+            connection.query(
+                "INSERT INTO department SET ?",
+            {
+                name: response.department, 
+            },
+            function(err){
+                if (err) throw err;
+                console.log("Department has been updated");
+                initialPrompt();
+            }
+        )
+        })
+};
 
 
